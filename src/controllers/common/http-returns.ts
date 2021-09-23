75b68957694f2ece01724ad53eb93dd556c97fbf
Tatiana Vitorello <vitorellotts@gmail.com>
2021-09-22 22:15:31 -0300

    refactor: remove unnused http return

--- src/controllers/common/http-returns.ts
+++ src/controllers/common/http-returns.ts
@@ -10,8 +10,3 @@ export const serverError = (error: Error): HttpResponse => ({
   status: 500,
   body: new ServerError(error.stack)
 })
-
-export const noContent = (): HttpResponse => ({
-  status: 204,
-  body: null
-})
