f2ef66ad5a4418583e3e1d71376015c68760aa28
Tatiana Vitorello <vitorellotts@gmail.com>
2021-09-22 21:09:54 -0300

    feat: initialize express app

---
+++ src/main/index.ts
@@ -0,0 +1,12 @@
+import 'module-alias'
+
+import { makeExpressApp } from '@/main/web/http/config/app'
+import env from './env'
+
+const main = async (): Promise<void> => {
+  const app = makeExpressApp()
+
+  app.listen(env.application.port, () => console.log(`Server listening on port ${env.application.port}! :)`))
+}
+
+main().catch(console.error)
