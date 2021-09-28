ea280661e644615ec20a14bf008d2193bb888bf6
Tatiana Vitorello <vitorellotts@gmail.com>
2021-09-27 21:06:06 -0300

    feat: create conversation route

--- src/main/web/http/routes/api-routes.ts
+++ src/main/web/http/routes/api-routes.ts
@@ -1,7 +1,9 @@
+import { makeContinueConversation } from '@/main/factories/conversation/continue-conversation/continue-conversation-factory'
 import { makeStartConversation } from '@/main/factories/conversation/start-conversation/start-conversation-factory'
 import { adaptExpressRoute } from '@/main/web/http/config/route-adapter'
 import { Router } from 'express'
 
 export default async (router: Router): Promise<void> => {
   router.post('/welcome', adaptExpressRoute(makeStartConversation()))
+  router.post('/conversation', adaptExpressRoute(makeContinueConversation()))
 }
